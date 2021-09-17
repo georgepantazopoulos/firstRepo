9ad7a90f7ffb46e8b472dcbc3c2b2a02982be126
georgepantazopoulos <georgetpantazo@gmail.com>
2021-09-17 14:26:22 -0400

    Made changes to the main method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -24,6 +24,10 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("johnny");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
 
