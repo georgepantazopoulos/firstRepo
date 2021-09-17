254f168154ee66e99a79df80470438acd36885ef
georgepantazopoulos <georgetpantazo@gmail.com>
2021-09-17 14:18:45 -0400

    Committing File

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList<BuddyInfo> addresses;
+
+    public AddressBook() {
+        this.addresses = new ArrayList<BuddyInfo>();
+    }
+
+    public String addBuddy(BuddyInfo buddy){
+        this.addresses.add(buddy);
+        return("Buddy Added");
+    }
+
+    public String removeBuddy(BuddyInfo buddy){
+        for(BuddyInfo b: this.addresses){
+            if (b.getName() == buddy.getName()){
+                this.addresses.remove(b);
+                return("Buddy Removed");
+            }
+        }
+        return("Buddy is not in Address Book");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+
+
+}
